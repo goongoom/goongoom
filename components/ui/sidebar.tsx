a45b7348f55add003d710a6fde5b6a7cde6259c4
Sunghyun Cho <hey@cho.sh>
2026-01-24 02:21:30 +0900

    fix: update Korean translations for clarity and conciseness in question security and restrictions

--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -521,7 +521,7 @@ function SidebarMenuButton({
       },
       props
     ),
-    render: !tooltip ? render : TooltipTrigger,
+    render,
     state: {
       slot: "sidebar-menu-button",
       sidebar: "menu-button",
@@ -542,7 +542,7 @@ function SidebarMenuButton({
 
   return (
     <Tooltip>
-      {comp}
+      <TooltipTrigger render={comp} />
       <TooltipContent
         side="right"
         align="center"
